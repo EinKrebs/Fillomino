36a6cc4e82d0465a068084219cc67e490979c08e
Yury <yura200253@gmail.com>
2020-11-20 17:47:56 +0500

    Added random_generate test

--- test_all.py
+++ test_all.py
@@ -4,6 +4,8 @@ import unittest
 from tests.puzzle_tests import PuzzleTests
 # noinspection PyUnresolvedReferences
 from tests.entry_tests import EntryTests
+# noinspection PyUnresolvedReferences
+from tests.generator_tests import GeneratorTests
 
 
 if __name__ == '__main__':
--- tests/generator_tests.py
+++ tests/generator_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 import generator
 
@@ -48,3 +49,26 @@ class GeneratorTests(unittest.TestCase):
                 [5, 4, 4, 5, 5],
                 [3, 4, 4, 5, 5]]
         self.assertFalse(generator.is_solution(test, 5, 5))
+
+    def test_generate_random(self):
+        random.seed(100)
+        for _ in range(10):
+            puzzle, solution = generator.random_generate(4, 4, 2, 5)
+            self.assertEqual(puzzle.width, 4)
+            self.assertEqual(puzzle.height, 4)
+            self.assertEqual(solution.width, 4)
+            self.assertEqual(solution.height, 4)
+            count = 0
+            arr = [[0 for _ in range(4)] for _ in range(4)]
+            for i in range(4):
+                for j in range(4):
+                    self.assertFalse(solution[i, j].empty)
+                    self.assertLessEqual(solution[i, j].value, 5)
+                    self.assertGreaterEqual(solution[i, j].value, 2)
+                    self.assertTrue(puzzle[i, j].empty
+                                    or puzzle[i, j] == solution[i, j])
+                    if not puzzle[i, j].empty:
+                        count += 1
+                    arr[i][j] = solution[i, j].value
+            self.assertTrue(generator.is_solution(arr, 4, 4))
+            self.assertLess(count, 16 * 0.4)
